ca195ef40ec0376da9f9f7721ee57cf4ce666d15
Markus Dietrich <celebrir@Celebriramba.(none)>
2012-05-16 13:53:28 +0200

    Djangobeispielviews und Templates

--- src/documents/views.py
+++ src/documents/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+# vim: set fileencoding=utf-8
+from models import *
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def authorbooks (request, author):
+    author_query = author.objects.filter(surname__icontains=s_author)
+    book_titles=[]
+    for document in author_query.objects.all():
+        book_titles.append(document.title)
+    response = HttpResponse("/n".join(book_titles))
+    response["Content-Type"] = "text/plain"
+    return response
+
+def templatebeispiel (request, author):
+    author_query = author.objects.filter(surname__icontains=s_author)
+    template = loader.get_template("Beispielbuchausgabe.html")
+    context = Context({"authoren" : author_query.objects.all()})
+    return HttpResponse(template.render(context))
