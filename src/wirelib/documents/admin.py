34398bf0f58e1b37484120097bfec9079ebe2fcd
Marco <marco.melzer@tu-bs.de>
2012-05-31 14:03:31 +0200

    kleine änderung

--- src/wirelib/documents/admin.py
+++ src/wirelib/documents/admin.py
@@ -2,12 +2,19 @@
 from documents.models import document
 from documents.models import user_profile
 from django.contrib import admin
+# from django.contrib.auth.models import User
+
+# class user_admin(admin.ModelAdmin):
+#    fields = ['username', 'password']
 
 class document_admin(admin.ModelAdmin):
-    fields = ['bib_no', 'category', 'publisher']
+    fields = ['bib_no', 'inv_no', 'bibtex_id', 'lib_of_con_nr', 'title',
+              'isbn', 'category', 'status', 'publisher',
+              'pub_date', 'address', 'price', 'currency','ub_date', 'comment']
 
 class user_profile_admin(admin.ModelAdmin):
     fields = ['user', 'street', 'number', 'zipcode', 'city']
 
+# admin.site.register(User,user_admin)
 admin.site.register(document, document_admin)
 admin.site.register(user_profile, user_profile_admin)
