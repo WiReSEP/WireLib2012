9e3aca726f7b78d53f2cfae74b95e72a050014fc
Marco <marco.melzer@tu-bs.de>
2012-05-31 12:17:09 +0200

    customize admin.py

--- src/wirelib/documents/admin.py
+++ src/wirelib/documents/admin.py
@@ -3,4 +3,11 @@ from documents.models import document
 from documents.models import user_profile
 from django.contrib import admin
 
-admin.site.register(document, user_profile)
+class document_admin(admin.ModelAdmin):
+    fields = ['bib_no', 'category', 'publisher']
+
+class user_profile_admin(admin.ModelAdmin):
+    fields = ['user', 'street', 'number', 'zipcode', 'city']
+
+admin.site.register(document, document_admin)
+admin.site.register(user_profile, user_profile_admin)
