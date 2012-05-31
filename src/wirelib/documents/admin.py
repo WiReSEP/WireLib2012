0628c429c02e0eb8f5d5ab2b2c46dad9ce3c5606
Marco <marco.melzer@tu-bs.de>
2012-05-31 11:55:23 +0200

    dokument & user auf adminsite

--- src/wirelib/documents/admin.py
+++ src/wirelib/documents/admin.py
@@ -1,4 +1,6 @@
+# vim: set fileencoding=utf-8
 from documents.models import document
+from documents.models import user_profile
 from django.contrib import admin
 
-admin.site.register(document)
+admin.site.register(document, user_profile)
