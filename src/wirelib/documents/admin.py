9e1bb94d0a8dbc9a5020a692cbfaaff2360b4b15
Markus Dietrich <markus.dietrich@tu.bs.de>
2012-05-31 15:31:37 +0200

    admin.py kann Dokumente erstellen

--- src/wirelib/documents/admin.py
+++ src/wirelib/documents/admin.py
@@ -1,20 +1,36 @@
 # vim: set fileencoding=utf-8
 from documents.models import document
 from documents.models import user_profile
+from documents.models import tel_user
+from documents.models import category
 from django.contrib import admin
-# from django.contrib.auth.models import User
+from documents.models import publisher
+from documents.models import author
 
-# class user_admin(admin.ModelAdmin):
-#    fields = ['username', 'password']
+class tel_user_inline(admin.StackedInline):
+    model = tel_user
+    extra = 1
+
+class category_admin(admin.ModelAdmin):
+    fields = ['name']
+
+class publisher_admin(admin.ModelAdmin):
+    fields = ['name']
+
+class author_admin(admin.ModelAdmin):
+    fields = ['documents', 'first_name', 'last_name']
 
 class document_admin(admin.ModelAdmin):
     fields = ['bib_no', 'inv_no', 'bibtex_id', 'lib_of_con_nr', 'title',
               'isbn', 'category', 'status', 'publisher',
-              'address', 'price', 'currency','ub_date', 'comment']
+              'address','year', 'price', 'currency', 'comment']
 
 class user_profile_admin(admin.ModelAdmin):
     fields = ['user', 'street', 'number', 'zipcode', 'city']
+    inlines = [tel_user_inline]
 
-# admin.site.register(User,user_admin)
+admin.site.register(publisher, publisher_admin)
+admin.site.register(author, author_admin)
+admin.site.register(category, category_admin)
 admin.site.register(document, document_admin)
 admin.site.register(user_profile, user_profile_admin)
