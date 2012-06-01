d59f73a2dec416635ba39bea218cad70b42e2939
Marco <marco.melzer@tu-bs.de>
2012-06-01 19:17:28 +0200

    Benutzer erstellen enthält alle Felder

--- src/wirelib/documents/admin.py
+++ src/wirelib/documents/admin.py
@@ -4,11 +4,14 @@ from documents.models import user_profile
 from documents.models import tel_user
 from documents.models import category
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from documents.models import publisher
 from documents.models import author
 
 class tel_user_inline(admin.StackedInline):
     model = tel_user
+    fk_name = 'user'
     extra = 1
 
 class category_admin(admin.ModelAdmin):
@@ -25,12 +28,17 @@ class document_admin(admin.ModelAdmin):
               'isbn', 'category', 'status', 'publisher',
               'address','year', 'price', 'currency', 'authors', 'comment']
 
-class user_profile_admin(admin.ModelAdmin):
-    fields = ['user', 'street', 'number', 'zipcode', 'city']
-    inlines = [tel_user_inline]
+class user_profile_inline(admin.StackedInline):
+    model = user_profile
+    fk_name = 'user'
+    max_num = 1
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [user_profile_inline, tel_user_inline]
 
 admin.site.register(publisher, publisher_admin)
 admin.site.register(author, author_admin)
 admin.site.register(category, category_admin)
 admin.site.register(document, document_admin)
-admin.site.register(user_profile, user_profile_admin)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
