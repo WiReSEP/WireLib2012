f49e41a09180b6a94dcbc9694c3a4a8b63e90016
Joern Hameyer <j.hameyer@tu-bs.de>
2012-07-11 15:08:51 +0200

    Einleitung Testdoku

--- src/wirelib/documents/tests.py
+++ src/wirelib/documents/tests.py
@@ -4,14 +4,20 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.test.client import Client
+from django.utils import unittest
 
-from django.test import TestCase
+class SimpleTest(unittest.TestCase):
+    def test_doc_details(self):
+        c = Client()
+        response = c.get('/doc_list/')
+        self.assertEqual(response.status_code, 200)
 
+#response = c.post('/login/', {'username': #'admin', 'password': 'sep2012'})
+#response.status_code
+    def test_sortlist(self):
+        c = Client
+        response = c.get('/doc_list/?&starts=g-i/')
+        self.assertEqual(response.status_code, 200)   
+    
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-# TODO: Check für migration
