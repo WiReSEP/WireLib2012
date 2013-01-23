59d100b2488d492a3c4539fe67c2dd628ad144ce
Philipp Offensand <philippoffensand@gmx.de>
2013-01-23 17:42:15 +0100

    bugfixes

--- settings/default.py
+++ settings/default.py
@@ -75,6 +75,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    normpath(join(DJANGO_ROOT, 'app', 'documents', 'static')),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -105,10 +106,10 @@ MIDDLEWARE_CLASSES = (
     'pagination.middleware.PaginationMiddleware',
 )
 
-ROOT_URLCONF = 'WireLib.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    normpath(join(JANGO_ROOT, 'templates')),
+    normpath(join(DJANGO_ROOT, 'templates')),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
