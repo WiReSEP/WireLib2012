13b495ee24dbd249e758d746f6eeb00d79d5a30c
Theodor van Nahl <theo_dev@van-nahl.org>
2013-06-03 12:56:26 +0200

    gunicorn added

--- settings/default.py
+++ settings/default.py
@@ -133,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'documents',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
