8d4a70556556822470e2877ced85edf56893f840
Theodor van Nahl <theo_dev@van-nahl.org>
2013-06-10 14:38:34 +0200

    Missing Import

--- app/documents/views/user_profile.py
+++ app/documents/views/user_profile.py
@@ -1,6 +1,7 @@
 #vim: set fileencoding=utf-8
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.template import Context, RequestContext
 from django.shortcuts import render_to_response
 from django.forms.models import modelformset_factory
