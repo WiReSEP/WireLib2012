97af91628e5542df1641a4c19ff2e8e5d42aef53
Theodor van Nahl <theo_dev@van-nahl.org>
2013-08-06 09:03:13 +0200

    new template development base

--- app/loremipsum/views.py
+++ app/loremipsum/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'loremipsum/base.html', None)
+
+
+def documents(request):
+    return render(request, 'loremipsum/doc_list.html', None)
+
+
+def documents_search(request):
+    return render(request, 'loremipsum/search.html', None)
+
+
+def new_documents(request):
+    return render(request, 'loremipsum/doc_add_modify.html', None)
+
+
+def export(request):
+    return render(request, 'loremipsum/export.html', None)
+
+
+def doc_detail(request):
+    return render(request, 'loremipsum/doc_detail.html', None)
