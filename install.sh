34d2053da70282b85d7ec6c201945c9926d66281
Theodor van Nahl <theo_dev@van-nahl.org>
2014-07-09 14:12:31 +0200

    reworks für das deployment wegen gunicorn

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USE_MYSQL=y
-DJANGO_VERSION=1.4
+DJANGO_VERSION=1.5
 ENGINE=
 USERNAME=
 PASSWORD=
@@ -10,70 +10,17 @@ WORKING_PATH=${0%/*}
 SENDER_MAIL=
 
 
-echo Welcome to the WireLib installation script
-echo ==========================================
-echo
-echo
-read -p "Do you wish to use MySQL as database?(Y/n):" USE_MYSQL
-USE_MYSQL=${USE_MYSQL:-y}
-if [[ $USE_MYSQL == 'y' ]];then
-  ENGINE=mysql
-  echo "Using MySQL"
-  while [ -z $USERNAME ]; do
-	read -p "Please enter the MySQL Username:" USERNAME
-  done
-  while [ -z $PASSWORD ]; do
-	read -p "Please enter the MySQL Username passwort:" PASSWORD
-  done
-  while [ -z $DATABASE ]; do
-	read -p "Please enter the MySQL Database:" DATABASE
-  done
-  read -p "Please enter the MySQL Host (default: localhost):" HOST
-  HOST=${HOST-localhost}
-  read -p "Please enter the MySQL Port (enter for default):" PORT
-else
-  ENGINE=sqlite3
-  echo "Using sqlite3 instead"
-  read -p "Please enter the path to the sqlite-file:" DATABASE
-fi
-while [ -z $SENDER_MAIL ]; do
-  read -p "Please enter the mail address for user notifications:" SENDER_MAIL
-done
-
-mkdir -p ${WORKING_PATH}/private/allegro
-mkdir -p ${WORKING_PATH}/private/bibtex
-
-echo "from settings.default import *
-DATABASES = {
-    'default': {
-	        'ENGINE': 'django.db.backends.${ENGINE}',
-			'NAME': '${DATABASE}',
-			'USER': '${USER}',
-			'PASSWORD': '${PASSWORD}',
-			'HOST': '${HOST}',
-			'PORT': '${PORT}',
-			}
-		}
-
-DEBUG = False
-
-DOCUMENTS_SECDIR = normpath(join(DJANGO_ROOT, 'private'))
-DOCUMENTS_SECDIR = '${WORKING_PATH}/private/'
-DOCUMENTS_BIBTEX = 'bibtex/'
-DOCUMENTS_ALLEGRO_FILES = 'allegro/'
-DOCUMENTS_SENDER_MAIL = '${SENDER_MAIL}'
-
-BIBTEX_DEBUG = False
-" > ${WORKING_PATH}/settings/deploy.py
-echo "from .default import *
-from .deploy import *" > ${WORKING_PATH}/settings/__init__.py
-
-# Virtualenv
-#
-
 cd ${WORKING_PATH}
+mkdir var
+cd var
 virtualenv -p python2 --system-site-packages virtualenv
 . virtualenv/bin/activate
 pip install django==${DJANGO_VERSION}
 pip install flup
 pip install gunicorn
+django-admin.py startproject deploy
+cd deploy
+ln -s ../../app/documents documents
+
+
+echo "Now please edit the file var/deploy/settings.py for your needs."
