25e16d95b59d4e31d677a60d873964a6f21be0b9
Theodor van Nahl <theo_dev@van-nahl.org>
2014-09-12 17:14:56 +0200

    Fixes #101 Benutzer können sich ohne Probleme ummelden.
    Vorher wurde der next Parameter immer auf die aktuelle Seite or einem Login
    gesetzt. Jetzt wird das nicht gemacht, wenn es sich um die logout Seite
    handelt.

--- app/users/context_processors.py
+++ app/users/context_processors.py
@@ -1,7 +1,14 @@
+from django.core.urlresolvers import reverse
+
+
 def user_context(request):
     return add_fullpath(request)
 
 
 def add_fullpath(request):
-    return {'fullpath': request.get_full_path(),
+    path = request.get_full_path()
+
+    is_logoutpage = reverse('users.logout') in path
+    return {'fullpath': path,
+            'is_logoutpage': is_logoutpage,
             }
